94022e1a505f96e0eae356096e7d75b03716df01
frostyausty <priceaustin62@gmail.com>
2020-12-12 21:23:21 -0600

    added login feature, and majority of the front-end

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ Branches:
 
 <!-- feature/create-post -->
 
-feature/comment
+<!-- feature/comment -->
 
 feature/login
 
---
+++ controllers/home-routes.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_url',
+            'blog_text',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('home', { posts });
+        })
+        .catch(err=> {
+            console.log(err);
+            res.status(500).json(err);
+        });   
+});
+
+router.get('/dashboard', (req, res) => {
+    if(!req.session.loggedIn) {
+        res.redirect('/login');
+        return;
+    }
+    console.log(req.session);
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_url',
+            'blog_text',
+            'created_at'
+        ],
+        where: {
+            user_id: req.session.user_id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('home', { posts });
+        })
+        .catch(err=> {
+            console.log(err);
+            res.status(500).json(err);
+        });   
+});
+
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
+});
+
+module.exports = router;
