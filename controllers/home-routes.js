f5ef5832b160c89ead0d008fc38f18bfa0f873b2
frostyausty <priceaustin62@gmail.com>
2020-12-12 23:16:15 -0600

    updated post and dashboard handlebars/routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -72,7 +72,7 @@ router.get('/dashboard', (req, res) => {
     })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('home', { posts });
+            res.render('dashboard', { posts });
         })
         .catch(err=> {
             console.log(err);
@@ -80,6 +80,52 @@ router.get('/dashboard', (req, res) => {
         });   
 });
 
+router.get('/post/:id', (req, res) => {
+    if(!req.session.loggedIn) {
+        res.redirect('/login');
+        return;
+    }
+    console.log(req.session);
+
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'post_url',
+        'blog_text',
+        'created_at'
+        ],
+      include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        //const post = dbPostData.map(post => post.get({ plain: true }));
+        res.render('post');
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
